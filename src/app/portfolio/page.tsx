428693694853d03e6dc4bd3ff0216441a29c4dcc
Facundo Uferer <juanfacundouf@gmail.com>
2025-05-18 00:14:51 -0300

    preparacion para prtfolio

---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Page() {
+
+
+  return (
+    <div className="flex flex-col min-h-screen p-4 max-w-2xl mx-auto">
+      <h2>Lista de portafolios</h2>
+    </div>
+  );
+}
