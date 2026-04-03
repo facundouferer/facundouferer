b644e5244579ba1eb0a042c1fb5830ac1bbd3bca
Facundo Uferer <juanfacundouf@gmail.com>
2026-04-03 02:19:14 -0300

    ci: add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://facundouferer.github.io',
 	i18n: {
 		defaultLocale: 'es',
 		locales: ['es', 'en'],
