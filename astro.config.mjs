de83d3c9c83a8499bc1f67cc8bda12944c0a7a6a
Facundo Uferer <juanfacundouf@gmail.com>
2026-04-03 02:28:11 -0300

    fix(ci): configure Pages action and Astro base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://facundouferer.github.io',
+	base: '/facundouferer',
 	i18n: {
 		defaultLocale: 'es',
 		locales: ['es', 'en'],
